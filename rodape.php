cedf9207ba0676f1fbdda514e98bf0392e6a31a8
adrianowarmling <adrianowarmling@unidavi.edu.br>
2018-04-04 19:14:15 -0300

    adicionado cabecalho conecta e rodape

---
+++ rodape.php
@@ -0,0 +1,20 @@
+            </div>
+        </div>
+
+        <center>
+            <div class="jumbotron">
+                <div class="container">
+                    <div class="col-md-4 col-xs-12">
+                        <p>O impeachment é uma manobra dos Estados Unidos para prejudicar a Mulher-Sapiens!</p>
+                    </div>
+                    <div class="col-md-4 col-xs-12">
+                        <p>As delações premiadas são um crime do Rodrigo Constantino para manipular os companheiros e companheiras!</p>
+                    </div>
+                    <div class="col-md-4 col-xs-12">
+                        <p>As manifestações são um ato de ódio da elite branca para desestabilizar as conquistas do trabalhadores!</p>
+                    </div>
+                </div>
+            </div>
+        </center>
+    </body>
+</html>
